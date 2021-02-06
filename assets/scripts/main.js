2a7c77274eeba86aa0aa1a977834cdbc00d6eb35
Nick Krolikowski <nkroliko@ucsd.edu>
2021-02-05 17:19:06 -0800

    t

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,12 +18,14 @@ document.getElementById("honk-btn").addEventListener("click", function(event){
 num.addEventListener("input",function(event){
     slide.value = this.value;
     aud.volume = this.value;
+    window.alert(this.value);
     event.preventDefault();
 });
 
 slide.addEventListener("input",function(event){
     num.value = this.value;
     aud.volume = this.value;
+    window.alert(this.value);
     event.preventDefault();
 });
 // TODO
