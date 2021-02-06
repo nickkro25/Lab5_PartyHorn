0d7018b2b7ca5fcc3fa4de158644ce5c82b2aa46
Nick Krolikowski <nkroliko@ucsd.edu>
2021-02-05 16:09:03 -0800

    added event listener to button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,8 @@
 // main.js
-window.alert('Welcome to my home page!');
+//window.alert('Welcome to my home page!');
+let slide = document.getElementById("volume-slider");
+let slideVal = slide.getAttribute("value");
+let aud = document.getElementById("horn-sound");
+aud.volume = 1.0;
+document.addEventListener("honk-btn").addEventListener("click", aud.play());
 // TODO
