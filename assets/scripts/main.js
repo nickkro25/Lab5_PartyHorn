03d2a1e438a0463a020a753e7a7514b116bf261e
Nick Krolikowski <nkroliko@ucsd.edu>
2021-02-05 16:37:56 -0800

    make slider equal to val and volume

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,19 @@
 // main.js
-window.alert('Added prevent default!');
+window.alert('Aaud sdf!');
+
 let slide = document.getElementById("volume-slider");
 let slideVal = slide.getAttribute("value");
+
+let num = document.getElementById("volume-number");
+let numVal = num.getAttribute("value");
+
 let aud = document.getElementById("horn-sound");
-aud.volume = 1.0;
+
+numVal = slideVal;
+slideVal = numVal;
+
+aud.volume = numVal / 100;
+
 document.getElementById("honk-btn").addEventListener("click", function(event){
     aud.play();
     event.preventDefault();
