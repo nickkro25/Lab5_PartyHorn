cfc2efca486e74b6daa4da32f4759aab91a66637
Nick Krolikowski <nkroliko@ucsd.edu>
2021-02-05 18:49:19 -0800

    volume

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,5 @@
 // main.js
-window.alert('divide by 100');
+window.alert('volume');
 
 let slide = document.getElementById("volume-slider");
 
@@ -19,16 +19,19 @@ document.getElementById("honk-btn").addEventListener("click", function(event){
 num.addEventListener("input",function(event){
     let val = this.value;
     aud.volume = val/100;
+    slide.value = val;
     //window.alert(this.value);
     event.preventDefault();
     if(this.value == 0){
         vImg.src = "./assets/media/icons/volume-level-0.svg";
+        document.getElementById("honk-btn").disabled = true;
     } else if(this.value < 34.0){
+        document.getElementById("honk-btn").disabled = false;
         vImg.src = "./assets/media/icons/volume-level-1.svg";
     } else if(this.value < 67.0){
         vImg.src = "./assets/media/icons/volume-level-2.svg";
     }else{
-        vImg.src = "./assets/media/icons/volume-level-4.svg";
+        vImg.src = "./assets/media/icons/volume-level-3.svg";
     }
 });
 
@@ -40,12 +43,14 @@ slide.addEventListener("input",function(event){
     event.preventDefault();
     if(val == 0){
         vImg.src = "./assets/media/icons/volume-level-0.svg";
+        document.getElementById("honk-btn").disabled = true;
     } else if(val < 34.0){
         vImg.src = "./assets/media/icons/volume-level-1.svg";
+        document.getElementById("honk-btn").disabled = false;
     } else if(val < 67.0){
         vImg.src = "./assets/media/icons/volume-level-2.svg";
     }else{
-        vImg.src = "./assets/media/icons/volume-level-4.svg";
+        vImg.src = "./assets/media/icons/volume-level-3.svg";
     }
 });
 // TODO
